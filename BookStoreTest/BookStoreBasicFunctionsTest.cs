54090c7efb453022e523ddc6207ea6ce73e6bd27
Saimer <saimer.nieves@gmail.com>
2022-02-08 20:23:39 -0500

    imported all files again

---
+++ BookStoreTest/BookStoreBasicFunctionsTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using MidtermPractical;
+namespace BookStoreTest
+{
+    public class BookStoreBasicFunctionsTest
+    {
+
+        [Fact]
+
+        public void GetBookByBookTitleTest()
+        {
+            var result = BookStoreBasicFunctions.GetAllBooksByBookTitle("The Travels of Marco Polo");
+
+            Assert.True(result.Count==1);
+
+        }
+
+
+        [Fact]
+
+        public void GetAllBooks()
+        {
+            var result = BookStoreBasicFunctions.GetAllBooks();
+
+            Assert.True(result.Count == 2);
+
+
+        }
+
+        [Fact]
+
+        public void GetAllBooksByAuthorLastNameTest()
+        {
+            var result = BookStoreBasicFunctions.GetAllBooksByAuthorLastName("polo");
+
+            Assert.True(result.Count == 1);
+
+        }
+
+
+
+
+    }
+}
